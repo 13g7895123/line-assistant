0cd25a5a4a51cf2a47c4579f3279002fb46f0b0d
Jarvis <13g7895123@gmail.com>
2022-11-07 08:10:41 +0800

    申請free ssl憑證

--- app.py
+++ app.py
@@ -2,8 +2,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return '123'
+    return '234'
 
-app.run()
+@app.route('/.well-known/pki-validation/<string:text_name>/')
+def ssl_test(str):
+    return '2344'
+
+@app.route('/.well-known/pki-validation/EB8B3233208300C9948562303D9F3A84.txt/')
+def EB8B3233208300C9948562303D9F3A84():
+    return render_template("EB8B3233208300C9948562303D9F3A84.txt")
+
+if __name__ == '__main__':
+    app.run()
