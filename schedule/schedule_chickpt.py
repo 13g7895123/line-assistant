49e89a4c88d7b5417442348728bc272362152ae2
Jarvis <13g7895123@gmail.com>
2022-11-09 21:46:55 +0800

    新增呼吸

--- schedule/schedule_chickpt.py
+++ schedule/schedule_chickpt.py
@@ -1,11 +1,37 @@
 from web_crawler.chickpt import *
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+import pymysql
+from config.db_mysql import *
+
+
+def chickpt_breathe():
+    now_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    db_config = config_db_config
+    conn = pymysql.connect(**db_config)
+    try:
+        with conn.cursor() as cursor:
+            command = "SELECT * FROM crawler_log WHERE NAME='chickpt'"
+            cursor.execute(command)
+            data = cursor.fetchall()
+            if len(data) > 0:
+                # 更新進LIST
+                command = "UPDATE crawler_log SET last_process_time = '" + str(now_time) + "' WHERE name = 'chickpt'"
+                cursor.execute(command)
+            else:
+                command = "INSERT INTO crawler_log(name, last_process_time) VALUES(%s, %s)"
+                cursor.execute(command, ('chickpt', str(now_time)))
+            conn.commit()
+    except Exception as ex:
+        print(ex)
+    finally:
+        conn.close()
 
 
 def schedule_chickpt():
     print("process running... %s" % datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     crawler_chickpt()
+    chickpt_breathe()
 
 
 scheduler = BlockingScheduler(timezone="Asia/Taipei")
