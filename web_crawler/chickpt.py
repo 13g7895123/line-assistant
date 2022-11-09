6978460d976e79474142fbbbb68d8c0a2901b787
Jarvis <13g7895123@gmail.com>
2022-11-09 19:35:59 +0800

    ubuntu debug

--- web_crawler/chickpt.py
+++ web_crawler/chickpt.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from config.web_address import *
+from config.web_address import address_chickpt
 from line_notify import *
 import pymysql
-from .config.db_mysql import *
-from .line_bot_push_message import *
+from config.db_mysql import *
+from line_bot_push_message import *
 
 
 def crawler_chickpt():
