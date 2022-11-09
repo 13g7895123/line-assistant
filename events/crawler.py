08cf7e96d8e2edf77d08c69ccb4801f5a0cbde56
Jarvis <13g7895123@gmail.com>
2022-11-09 22:29:29 +0800

    debug

--- events/crawler.py
+++ events/crawler.py
@@ -4,22 +4,21 @@ from config.db_mysql import *
 
 
 def breathe_chickpt(event):
-    msg = 'test'
+    db_config = config_db_config
+    conn = pymysql.connect(**db_config)
+    try:
+        with conn.cursor() as cursor:
+            command = "SELECT * FROM crawler_log WHERE NAME='chickpt'"
+            cursor.execute(command)
+            data = cursor.fetchall()
+            if len(data) > 0:
+                update_time = data[0][2]
+
+    except Exception as ex:
+        print(ex)
+    finally:
+        conn.close()
+
+    msg = '【小雞上工：查詢上次更新時間】\n' + str(update_time)
     text_message = TextSendMessage(msg)
     line_bot_api.reply_message(event.reply_token, text_message)
-    # db_config = config_db_config
-    # conn = pymysql.connect(**db_config)
-    # try:
-    #     with conn.cursor() as cursor:
-    #         command = "SELECT * FROM crawler_log WHERE NAME='chickpt'"
-    #         cursor.execute(command)
-    #         data = cursor.fetchall()
-    #         if len(data) > 0:
-    #             update_time = data[0][2]
-    #             msg = '【小雞上工：查詢上次更新時間】\n' + str(update_time)
-    #             text_message = TextSendMessage(msg)
-    #             line_bot_api.reply_message(event.reply_token, text_message)
-    # except Exception as ex:
-    #     print(ex)
-    # finally:
-    #     conn.close()
