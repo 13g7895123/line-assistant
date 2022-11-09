f4c2457caadc20aeff94b5590e644b9f8d6d885b
Jarvis <13g7895123@gmail.com>
2022-11-09 22:40:08 +0800

    debug - 查詢資料與主體分離

--- events/crawler.py
+++ events/crawler.py
@@ -3,7 +3,7 @@ import pymysql
 from config.db_mysql import *
 
 
-def breathe_chickpt(event):
+def get_data():
     db_config = config_db_config
     conn = pymysql.connect(**db_config)
     try:
@@ -13,13 +13,16 @@ def breathe_chickpt(event):
             data = cursor.fetchall()
             if len(data) > 0:
                 update_time = data[0][2]
-
+                return update_time
     except Exception as ex:
         print(ex)
     finally:
         conn.close()
 
-    # msg = '【小雞上工：查詢上次更新時間】\n' + str(update_time)
-    msg = 'testt'
+
+def breathe_chickpt(event):
+    update_time = get_data()
+    msg = '【小雞上工：查詢上次更新時間】\n' + str(update_time)
+    # msg = 'testt'
     text_message = TextSendMessage(msg)
     line_bot_api.reply_message(event.reply_token, text_message)
