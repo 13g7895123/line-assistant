f15210f3c3917497ec662c0dad1fb4d6c2cb40c8
Jarvis <13g7895123@gmail.com>
2022-11-10 12:02:00 +0800

    debug

--- events/crawler.py
+++ events/crawler.py
@@ -1,10 +1,10 @@
 from line_bot_api import *
-from app import *
-
-
-def breathe_chickpt(event):
-    command = "SELECT * FROM crawler_log WHERE NAME='chickpt'"
-    data = db.engine.execute(command)
-    msg = str('【小雞上工：查詢上次更新時間】\n' + str(data))
-    text_message = TextSendMessage(msg)
-    line_bot_api.reply_message(event.reply_token, text_message)
+# from app import *
+#
+#
+# def breathe_chickpt(event):
+#     command = "SELECT * FROM crawler_log WHERE NAME='chickpt'"
+#     data = db.engine.execute(command)
+#     msg = str('【小雞上工：查詢上次更新時間】\n' + str(data))
+#     text_message = TextSendMessage(msg)
+#     line_bot_api.reply_message(event.reply_token, text_message)
