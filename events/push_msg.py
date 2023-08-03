e9da57422585968a09ee7f7a3f01202f80bbc232
Jarvis <13g7895123@gmail.com>
2023-08-03 11:42:51 +0800

    test

--- events/push_msg.py
+++ events/push_msg.py
@@ -3,5 +3,5 @@ from line_bot_api import *
 def push_msg(event):
     uid = event.source.userId
 
-    text_message = TextSendMessage(uid)
+    text_message = TextSendMessage('123')
     line_bot_api.reply_message(event.reply_token, text_message)
