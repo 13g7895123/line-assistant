87130a709e5066748365f43c12e7bc46ebf6ff9b
Jarvis <13g7895123>
2023-08-03 11:23:07 +0800

    [edit] update old files

--- events/web.py
+++ events/web.py
@@ -8,9 +8,13 @@ def weather(event, message_text):
     api_key = 'b85f1d04ed29cb2e9f8acc9c041658b0'
     address = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
 
-    data = requests.get(address)
-    print(round(data.json()['main']['temp'] - 273.15, 2))
-    template = round(data.json()['main']['temp'] - 273.15, 2)
+    try:
+        data = requests.get(address)
+        print(round(data.json()['main']['temp'] - 273.15, 2))
+        template = round(data.json()['main']['temp'] - 273.15, 2)
+        msg = f'現在{city}的溫度是{template}°C'
+    except Exception as e:
+        msg = f'查詢錯誤，請確認城市名稱'
 
-    text_message = TextSendMessage(f'現在{city}的溫度是{template}°C')
+    text_message = TextSendMessage(msg)
     line_bot_api.reply_message(event.reply_token, text_message)
